03245b193ab391e9f8548d3a3f1772a8cf594e55
jawn1890 <jawn1890@gmail.com>
2024-02-21 15:56:25 -0500

    edited Ball Controller script to display a debug log and removed gravity from the ball.

--- pong_JonathanG001/Assets/Scripts/Ball Controller.cs
+++ pong_JonathanG001/Assets/Scripts/Ball Controller.cs
@@ -7,7 +7,7 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("H3ll0 W3rld");
     }
 
     // Update is called once per frame
